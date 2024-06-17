364c6fe6816781cd17203dc158c3b2bb86d4c0ff
Mayank <Mayank>
2024-06-17 18:11:15 +0530

    new

---
+++ code/index.js
@@ -0,0 +1,17 @@
+const age=24;
+const name='Aman Badalia'
+const dob='01/11/2001'
+const title="Git"
+function name(){
+    console.log('My name is Aman')
+}
+
+function isAbove18(){
+    return true;
+}
+
+function getname(){
+    return "Sam  jebf" ;
+}
+
+
